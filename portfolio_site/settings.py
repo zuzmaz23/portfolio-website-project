84245f7a9160d647a7f366c9e599e67f27f922a8
Zuzanna Mazur <ZuzannaMazur@interia.pl>
2026-01-26 10:07:03 +0100

    add aplication configuration

--- portfolio_site/settings.py
+++ portfolio_site/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$e=^0y)x9@_&1*sb8p+7@92%z_sae(7oqm%*68hwke=v9)fkh!'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY'"django-insecure-$e=^0y)x9@_&1*sb8p+7@92%z_sae(7oqm%*68hwke=v9)fkh!")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', 'True')=='True'
+
 
 ALLOWED_HOSTS = []
 
