947601a50bebd366c281e41e8717e813982f0301
zuzmaz23 <ZuzannaMazur@interia.pl>
2026-02-05 12:19:58 +0100

    add home view

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import About, Technology, Project 
+from .forms import ContactForm
 
-# Create your views here.
+def home(request):
+
+    about = About.get_about()
+
+    technologies = Technology.objects.all()
+
+    projects = Project.objects.prefetch-related('technologies').all()
+
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Dziękuję za wiadomość! Odpowiem najszybciej jak to możliwe.')
+            return redirect('home')
+    else:
+        form = ContactForm()
+    
+    context =(
+        'about': about,
+        'technologies': technologies,
+        'projects': projects,
+        'from': form
+    )
+
+    return render(request, 'portfolio/home.html', context)
